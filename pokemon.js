185ac8219a5080b50c197c25ea740b19fa9537ce
Nicole Dutschmann <n.kiak@gmx.de>
2023-09-05 13:01:25 +0200

    first

---
+++ pokemon.js
@@ -0,0 +1,217 @@
+const emoji1 = String.fromCodePoint(0x1f621);
+const emoji2 = String.fromCodePoint(0x1f929);
+const emoji3 = String.fromCodePoint(0x1f49a);
+const emoji4 = String.fromCodePoint(0x1f4a5);
+const emoji5 = String.fromCodePoint(0x1f91d);
+function picture(string) {
+  if (string == "Relaxo" || string == "Relaxo") {
+    return `\n         ⢠⣤⣀   ⢀⣀⣤⣤⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⢀⢀⠀⠀⠀⢸⡿⠛⠛⠛⠛⠛⠉⠛⢿⣿⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠠⣿⣿⣿⣄⠀⣼⠀⠀⠀⢂⣀⣀⡀⠀⠀⢹⡀⠀⠀⠀⠀⠀⠀
+  ⠀⢸⣿⣿⣿⣿⡷⠋⠈⠀⠀⠀⠀⠀⠀⠀⠈⠘⠣⡀⠀⠀⠀⠀⠀
+  ⠀⠈⣿⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣷⣦⡀⠀⠀
+  ⠀⠀⢹⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⣦⠀
+  ⠀⠀⣸⣿⣿⣶⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇
+  ⠀⣤⡟⠛⠋⠉⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠉⠈⠋⠈⢿⣿⡿
+  ⢀⡉⠀⠀⣀⣤⣄⢈⣿⣿⣿⣿⣿⣿⣿⣿⣿⢀⣤⣤⣄⠀⠀⣴⡄
+  ⠘⢇⠀⠰⣿⣿⢟⢼⣿⣿⣿⣿⣿⣿⣿⣿⡿⢜⠿⠿⠿⠀⡀⠀⠀
+  ⠀⠀⠁⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠀⠀⠈⠀⠀⠀\n`;
+  }
+  if (string == "Schiggy" || string == "Schiggy") {
+    return `\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠤⠐⠒⠒⠂⠠⡀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠊⠀⠀⡠⢠⠂⠀⠀⠀⠡⡀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠇⠀⠀⢰⣷⣾⠀⠀⠀⠀⠀⡇⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠜⢨⠢⠔⡀⠀⠠⠘⠛⠛⠀⠀⠀⠀⢸⡇⠀
+⠀⠀⠀⢀⣀⣀⠀⠀⠀⠰⠀⠀⠀⠀⠡⡀⠀⠈⠀⠒⠂⠄⡀⢀⠀⡀⠀
+⠀⡴⠊⠀⠀⠀⠉⢆⠀⡔⢣⠀⠀⠀⠀⠐⡤⣀⠀⠀⠀⠀⠀⣀⠄⠀⠀
+⢸⠀⠀⠀⢠⠀⠀⠈⣼⠀⠀⠣⠀⠀⠀⡰⡀⠀⠉⠀⠀⠰⠉⠀⠁⠠⢄
+⢰⠀⠀⠀⠀⠇⠀⢀⢿⠀⢀⠇⡐⠀⠈⠀⠈⠐⠠⠤⠤⠤⠀⠀⠀⠀⢨
+⠀⢓⠤⠤⠊⠀⠀⢸⠀⠣⠀⡰⠁⠀⠀⡀⠀⠀⠀⠸⠀⢰⠁⠐⠂⠈⠁
+⠀⠀⠑⢀⠀⠀⠀⠈⣄⠖⠉⠑⢄⠠⠊⠀⠢⢄⣠⣃⣀⡆⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠑⠠⢀⣀⠎⠀⠀⠀⠈⡄⠀⠀⠀⢠⢃⠠⠃⠐⡀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠸⠀⠀⠀⠀⢀⠯⠉⠤⢴⡃⠁⠀⠀⠀⡇⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠰⡁⠀⠀⠀⠠⠂⠀⠀⠀⠀⠑⢄⠀⠀⢀⠲⠁⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠘⠒⠑⠔⠁⠀⠀⠀⠀⠀⠀⠀⠁⠉⠀⠀⠀⠀⠀⠀\n`;
+  }
+  if (string == "Enton" || string == "Enton") {
+    return `\n⠀⠀⠀⠀⠀⠀⠀⠀⣤⡀⠀⣶⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣆⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠸⣷⣮⣿⣿⣄⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢀⡠⠒⠉⠀⠀⠀⠀⠀⠀⠈⠁⠲⢖⠒⡀⠀⠀
+⠀⠀⠀⡠⠴⣏⠀⢀⡀⠀⢀⡀⠀⠀⠀⡀⠀⠀⡀⠱⡈⢄⠀
+⠀⠀⢠⠁⠀⢸⠐⠁⠀⠄⠀⢸⠀⠀⢎⠀⠂⠀⠈⡄⢡⠀⢣
+⠀⢀⠂⠀⠀⢸⠈⠢⠤⠤⠐⢁⠄⠒⠢⢁⣂⡐⠊⠀⡄⠀⠸
+⠀⡘⠀⠀⠀⢸⠀⢠⠐⠒⠈⠀⠀⠀⠀⠀⠀⠈⢆⠜⠀⠀⢸
+⠀⡇⠀⠀⠀⠀⡗⢺⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⡄⢀⠎
+⠀⢃⠀⠀⠀⢀⠃⢠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠷⡃⠀
+⠀⠈⠢⣤⠀⠈⠀⠀⠑⠠⠤⣀⣀⣀⣀⣀⡀⠤⠒⠁⠀⢡⠀
+⡀⣀⠀⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢘⠀
+⠑⢄⠉⢳⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡸⠀
+⠀⠀⠑⠢⢱⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠁⠀
+⠀⠀⠀⠀⢀⠠⠓⠢⠤⣀⣀⡀⠀⠀⣀⣀⡀⠤⠒⠑⢄⠀⠀
+⠀⠀⠀⠰⠥⠤⢄⢀⡠⠄⡈⡀⠀⠀⣇⣀⠠⢄⠀⠒⠤⠣⠀
+⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀\n`;
+  }
+  if (string == "Pikachu" || string == "Pikachu") {
+    return `\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⣴⡿⠁⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠊⠀⡿⠁⠀⠀⠀⠀⠀
+⠀⠀⢀⣤⣤⡤⠤⠤⠤⢀⣀⡤⠒⠂⠈⠉⠁⠒⢡⡀⡔⠁⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠉⠛⠿⢦⡀⠀⠀⠀⢀⢄⡀⠀⠀⠀⢠⣲⡙⡄⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠈⠁⠖⠀⠸⠾⠃⠀⠠⠄⠈⠋⠁⡵⣠⠤⡄⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⡤⣐⡀⠔⠉⢢⠀⠢⠔⠳⡆⠁⠀⢄⠇⢀⠇⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠻⡀⠈⠑⢌⠒⠊⠀⠀⠁⠈⠀⠀⡠⠊⠀⡘⠀⠀⠀⠀⠀
+⡖⠒⠢⠤⠤⠤⠤⢵⡀⠀⠀⠑⠀⠀⠀⠂⠒⠀⠀⠀⠀⢠⠁⠀⠀⠀⠀⠀
+⡇⠀⠀⠀⠀⠀⠀⠠⠱⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡃⠀⠀⠀⠀⠀⠀
+⡄⠀⠀⣀⡀⠀⣠⡣⡆⡘⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀
+⠉⠉⠁⠀⡆⠀⣩⠠⡗⡇⣸⠟⢣⡀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠓⠉⢸⡰⣧⠽⡈⡄⢸⠀⠀⠀⠀⠀⠀⠀⠀⠇⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠑⠴⢜⣀⠀⠤⢄⣀⠀⢀⠜⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢡⠈⡇⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢲⠆⠀⠠⡤⠄⠤⣦⠀⠠\n`;
+  }
+  if (string == "Glumanda" || string == "Glumanda") {
+    return `\n⣀⠔⠒⠒⠂⢄
+⠀⠀⢰⢅⠀⠀⢀⣤⢄⢂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⣾⡆⠀⠀⠀⢸⠼⡎⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⢀⢗⠂⠀⠀⡀⠈⢉⠅⠇⠀⠀⠀⠀⠀⠀⢠⣄⠀
+⠀⠈⠢⣓⠔⣲⠖⡫⠊⡘⠀⠀⠀⠀⠀⠀⠲⡟⠙⡆
+⠀⢀⢀⠠⠘⣇⠖⢄⠀⠉⠐⠠⢄⣀⡀⠀⠜⠀⠀⣁
+⠘⣏⣀⣀⣀⠃⠀⠀⠑⣀⣀⣀⣰⠼⠇⠈⠄⠀⠈⡻
+⠀⠁⠀⠀⢰⠀⠀⠀⠀⠠⠀⠡⡀⠀⠀⠀⠈⡖⠚⠀
+⠀⠀⠀⡠⠘⠀⠀⠀⠀⢀⠆⠀⠐⡀⠀⡠⠊⣠⠀⠀
+⠀⠀⢐⠀⠀⠁⡀⠀⠀⢀⠀⠀⠀⢨⠀⡠⡴⠂⠀⠀
+⠀⢀⣨⣤⠀⠀⠐⠃⠐⠚⠢⠀⠀⠈⠑⠊⠀⠀⠀⠀
+⠀⠘⠓⠋⠉⠁⠀⠀⠀⠀⠀⠓⢶⡾⠗⠀⠀⠀⠀⠀\n`;
+  }
+}
+class Pokemon {
+  constructor(name, typ, hp, ap, skill) {
+    this.name = name;
+    this.typ = typ;
+    this.hp = hp;
+    this.ap = ap;
+    this.skill = skill;
+    this.initialHp = hp;
+    this.initialAp = ap;
+    this.wins = 0;
+    this.losses = 0;
+  }
+  fight(poke2) {
+    // console.clear();
+    console.log(
+      "\x1b[0m",
+      `___________________________________________________________`
+    );
+    console.log("\x1b[33m%s\x1b[0m", `${this.name} vs ${poke2.name}`);
+    this.info();
+    poke2.info();
+    console.log(
+      "\x1b[0m",
+      `___________________________________________________________`
+    );
+    while (poke2.hp >= 100 && this.hp >= 100 && this.ap > 0 && poke2.ap > 0) {
+      poke2.hp = poke2.hp - this.ap;
+      this.ap = this.ap - 50;
+      this.hp = this.hp - poke2.ap;
+      poke2.ap = poke2.ap - 50;
+      console.log(
+        `${this.name} attacks ${poke2.name} with ${this.skill} =>\n`,
+        `${poke2.name} hp =`,
+        poke2.hp,
+        `\n`,
+        `${this.name} ap =`,
+        this.ap
+      );
+      console.log(
+        `${poke2.name} attacks ${this.name} with ${poke2.skill} <=\n`,
+        `${this.name} hp =`,
+        this.hp,
+        `\n`,
+        `${poke2.name} ap =`,
+        poke2.ap
+      );
+    }
+    if (poke2.hp <= 100) {
+      console.log(
+        "\x1b[33m%s\x1b[0m",
+        `\n ${poke2.name} is defeated!${emoji1} \n ${this.name} has won the fight.${emoji2}`
+      );
+      poke2.resetHpAp();
+      this.resetHpAp();
+      this.wins++;
+      poke2.losses++;
+    }
+    if (this.hp <= 100) {
+      console.log(
+        "\x1b[33m%s\x1b[0m",
+        `\n ${this.name} is defeated!${emoji1} \n ${poke2.name} has won the fight. ${emoji2}`
+      );
+      this.resetHpAp();
+      poke2.resetHpAp();
+      poke2.wins++;
+      this.losses++;
+    }
+    if (this.ap <= 0 && poke2.ap <= 0) {
+      poke2.resetHpAp();
+      this.resetHpAp();
+      console.log(
+        "\x1b[33m%s\x1b[0m",
+        `\n ${this.name} and ${poke2.name} don't have any ap left. \n The fight is over. ${emoji5}`
+      );
+    }
+    if (this.ap <= 0) {
+      console.log(
+        "\x1b[33m%s\x1b[0m",
+        `\n ${this.name} can't attack anymore.${emoji1} \n ${poke2.name} has won the fight. ${emoji2}`
+      );
+      poke2.resetHpAp();
+      this.resetHpAp();
+      poke2.wins++;
+      this.losses++;
+    }
+    if (poke2.ap <= 0) {
+      console.log(
+        "\x1b[33m%s\x1b[0m",
+        `\n ${poke2.name} can't attack anymore.${emoji1} \n ${this.name} has won the fight. ${emoji2}`
+      );
+      this.resetHpAp();
+      poke2.resetHpAp();
+      this.wins++;
+      poke2.losses++;
+    }
+    console.log(
+      "\x1b[0m",
+      `___________________________________________________________`
+    );
+  }
+  info() {
+    console.log(picture(this.name));
+    console.log(
+      "\x1b[34m%s\x1b[0m",
+      `${this.name} is a Pokemon from type ${this.typ}. Has the following attributes: ${this.hp}hp${emoji3} , ${this.ap}ap${emoji4} and Skill: ${this.skill}.`,
+      "\x1b[0m"
+    );
+  }
+  resetHpAp() {
+    this.hp = this.initialHp;
+    this.ap = this.initialAp;
+  }
+  statistic() {
+    // console.clear();
+    console.log(picture(this.name));
+    console.log(
+      "\x1b[37m%s\x1b[1m",
+      `${this.name} has ${this.wins} wins and ${this.losses} losses.`
+    );
+  }
+}
+
+const pikachu = new Pokemon("Pikachu", "Electro", 1500, 300, "Thunderbolt");
+const enton = new Pokemon("Enton", "Water", 1200, 250, "Psycho ray");
+const schiggy = new Pokemon("Schiggy", "Water", 1300, 300, "Aqua gun");
+const glumanda = new Pokemon("Glumanda", "Fire", 1350, 150, "Flying sparks");
+const relaxo = new Pokemon("Relaxo", "Normal", 2000, 350, "Ram boss");
+
+
+enton.fight(schiggy)
+pikachu.fight(relaxo)
